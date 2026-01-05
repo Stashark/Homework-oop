bc39da733b8a5140d22b425fcb63c18344dc8e46
Anastasia Logacheva <stashark123@gmail.com>
2026-01-06 01:30:23 +1000

    Task_4

--- main.py
+++ main.py
@@ -23,12 +23,11 @@ class Student:
         return round(sum(all_grades) / len(all_grades), 1)
 
     def __str__(self):
-        return (f"Имя: {self.name}"
-                f"Фамилия: {self.surname}"
-                f"Средняя оценка за домашние задания: {self.average_grades}"
-                f"Курсы в процессе изучения: {self.courses_in_progress}"
-                f"Завершенные курсы:{self.finished_courses}"
-                )
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за домашние задания: {self.average_grades()}\n"
+                f"Курсы в процессе изучения: {self.courses_in_progress}\n"
+                f"Завершенные курсы: {self.finished_courses}\n")
 
     def __eq__(self, other):
         return self.average_grades() == other.average_grades()
@@ -50,7 +49,7 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.grades ={}
+        self.grades = {}
 
     def average_grades(self):
         all_grades = [grade for grades_list in self.grades.values() for grade in grades_list]
@@ -59,8 +58,9 @@ class Lecturer(Mentor):
         return round(sum(all_grades) / len(all_grades), 1)
 
     def __str__(self):
-        average_grades = self.average_grades
-        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_grades}'
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {self.average_grades()}')
 
     def __eq__(self, other):
         return self.average_grades() == other.average_grades()
@@ -87,3 +87,66 @@ class Reviewer(Mentor):
 
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}'
+
+
+def students_average_grades(students, course):
+    students_grades = []
+    for student in students:
+        grade = student.grades.get(course)
+        if grade:
+            students_grades += grade
+    if students_grades:
+        return round(sum(students_grades) / len(students_grades), 1)
+    else:
+        return None
+
+
+def lecturers_average_grades(lecturers, course):
+    lecturers_grades = []
+    for lecturer in lecturers:
+        grade = lecturer.grades.get(course)
+        if grade:
+            lecturers_grades += grade
+    if lecturers_grades:
+        return round(sum(lecturers_grades) / len(lecturers_grades), 1)
+    else:
+        return None
+
+
+student1 = Student('Петр', 'Ефремов', 'Мужской')
+student2 = Student('Екатерина', 'Соколова', 'Женский')
+
+lecturer1 = Lecturer('Сергей', 'Семечков')
+lecturer2 = Lecturer('Елена', 'Котик')
+
+reviewer1 = Reviewer('Алексей', 'Егоров')
+reviewer2 = Reviewer('Ольга', 'Иванова')
+
+
+student1.courses_in_progress.append('Python')
+student2.courses_in_progress.append('Java')
+
+
+lecturer1.courses_attached.append('Python')
+lecturer2.courses_attached.append('Java')
+
+reviewer1.courses_attached.append('Python')
+reviewer2.courses_attached.append('Java')
+
+
+student1.rate_lecture(lecturer1, 'Python', 9)
+student2.rate_lecture(lecturer2, 'Java', 8)
+
+reviewer1.rate_hw(student1, 'Python', 7)
+reviewer2.rate_hw(student2, 'Java', 6)
+
+
+print(f'Средняя оценка Петра Ефремова: {student1.average_grades()}')
+print(f'Средняя оценка Екатерины Соколовой: {student2.average_grades()}')
+
+print(student1)
+print(student2)
+print(lecturer1)
+print(lecturer2)
+print(reviewer1)
+print(reviewer2)
